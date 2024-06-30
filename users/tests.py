f1310501659463f0925bd978fe0b6b75ca63cdad
egor <sharapov.egor00@gmail.com>
2024-06-30 18:12:29 +0300

    add tests

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,22 @@
 from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+
+from users.models import User
 
 
-class CourseTestCase(APITestCase):
-    def setUp(self) -> None:
-        super().setUp()
+class UserTestCase(APITestCase):
+
+    def test_registration_user(self):
+        """
+        Тестирование регистрации пользователя
+        """
+        url = reverse("users:registration")
+        data = {"telegram_id": "12345", "chat_id": "12345", "password": "12345"}
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def tearDown(self):
+        super().tearDown()
+        for user in User.objects.all():
+            user.delete()
